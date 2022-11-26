449315b173151c81bd98f003bc832bcac8c7dcd9
Diocechen <78402906+Diocechen@users.noreply.github.com>
2022-11-26 11:05:56 +0800

    Add files via upload

---
+++ Class2.java
@@ -0,0 +1,15 @@
+package myJava13;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		int num = 12, den = 0;
+		if(den == 0) {
+			System.out.println(0);
+		}else {
+			int ans = num/den;
+			System.out.println(ans);
+		}
+	}
+
+}
